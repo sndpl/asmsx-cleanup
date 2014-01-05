84a786abe7dd97fb13716bf14b387f87abded19c
oboroc <aoboroc@gmail.com>
2014-01-05 17:59:56 -0500

    Switch warning level for Visual C++
    from /W2 to /W4

--- src/build-vc9.cmd
+++ src/build-vc9.cmd
@@ -15,7 +15,7 @@ flex.exe -i -Pparser2 -oparser2.c parser2.l
 flex.exe -i -Pparser3 -oparser3.c parser3.l
 bison.exe core.y -ocore.c -d
 flex.exe -i -olex.c lex.l 
-cl.exe /W2 asmsx.c core.c parser1.c parser2.c parser3.c wav.c > asmsx.vc9.err
+cl.exe /W4 asmsx.c core.c parser1.c parser2.c parser3.c wav.c > asmsx.vc9.err
 rem /W1 to /W4 for warnings, /WX to treat warning as errors, /O2 for moderate optimization
 
 del core.c core.h lex.c parser?.c *.obj
