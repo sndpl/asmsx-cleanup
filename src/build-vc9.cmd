bd65e8dfb78d8ae577eb8dc857c9576f63ace339
oboroc <aoboroc@gmail.com>
2014-01-11 20:43:17 -0500

    Change MSVC warning level from /W3 to /W4

--- src/build-vc9.cmd
+++ src/build-vc9.cmd
@@ -15,7 +15,7 @@ flex.exe -i -Pparser2 -oparser2.c parser2.l
 flex.exe -i -Pparser3 -oparser3.c parser3.l
 bison.exe core.y -ocore.c -d
 flex.exe -i -olex.c lex.l
-cl.exe /nologo /W3 asmsx.c core.c lex.c parser1.c parser2.c parser3.c wav.c > asmsx.vc9.err
+cl.exe /nologo /W4 asmsx.c core.c lex.c parser1.c parser2.c parser3.c wav.c > asmsx.vc9.err
 rem /W1 to /W4 for warnings, /WX to treat warning as errors, /O2 for moderate optimization
 
 del core.c core.h lex.c parser?.c *.obj
