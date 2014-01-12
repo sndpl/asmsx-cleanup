85fcd9f6b3d230e0bd0123ed963ab5c8b6319266
oboroc <aoboroc@gmail.com>
2014-01-11 23:57:32 -0500

    Change build scripts
    compile wav.c right after asmsx.c and before flex/bison generated files;
    call bison with extra flags "-v -b y"

--- src/build-vc6wfb.cmd
+++ src/build-vc6wfb.cmd
@@ -6,16 +6,16 @@ set INCLUDE=%VCDIR%\VC98\INCLUDE;..\..\compat_s\src
 set LIB=%VCDIR%\VC98\LIB
 set PATH=%VCDIR%\Common\msdev98\bin;%VCDIR%\VC98\bin;%PATH%
 
-set wfb=C:\win_flex_bison-2.4.1
-rem set wfb=C:\win_flex_bison-2.5.1
+rem set wfb=C:\win_flex_bison-2.4.1
+set wfb=C:\win_flex_bison-2.5.1
 set PATH=%PATH%;%wfb%
 
 win_flex.exe --wincompat -i -Pparser1 -oparser1.c parser1.l
 win_flex.exe --wincompat -i -Pparser2 -oparser2.c parser2.l
 win_flex.exe --wincompat -i -Pparser3 -oparser3.c parser3.l
-win_bison.exe core.y -ocore.c -d
+win_bison.exe -d -v -b y core.y -ocore.c
 win_flex.exe --wincompat -i -olex.c lex.l
-cl.exe /nologo /G6 /W4 asmsx.c core.c lex.c parser1.c parser2.c parser3.c wav.c > asmsx.vc6.err
+cl.exe /nologo /TC /G6 /W4 asmsx.c wav.c core.c lex.c parser1.c parser2.c parser3.c > asmsx.vc6wfb.err
 rem /W1 to /W4 for warnings, /WX to treat warning as errors, /O2 for moderate optimization
 
 del core.c core.h lex.c parser?.c *.obj *.i
--- src/build-wc19.cmd
+++ src/build-wc19.cmd
@@ -11,16 +11,16 @@ set PATH=%PATH%;%mingw%\bin;%mingw%\msys\1.0\bin
 flex.exe -i -Pparser1 -oparser1.c parser1.l
 flex.exe -i -Pparser2 -oparser2.c parser2.l
 flex.exe -i -Pparser3 -oparser3.c parser3.l
-bison.exe core.y -ocore.c -d
+bison.exe -d -v -b y core.y -ocore.c
 flex.exe -i -olex.c lex.l
 
 wcc386.exe /W4 asmsx.c
+wcc386.exe /W4 wav.c
 wcc386.exe /W4 core.c
 wcc386.exe /W4 lex.c
 wcc386.exe /W4 parser1.c
 wcc386.exe /W4 parser2.c
 wcc386.exe /W4 parser3.c
-wcc386.exe /W4 wav.c
 link386.exe asmsx.obj core.obj lex.obj parser1.obj parser2.obj parser3.obj wav.obj
 
 del core.c core.h lex.c parser?.c *.obj
