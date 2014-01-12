11c83e7eaaf5fb0b472b22f6102b84aca168c84d
oboroc <aoboroc@gmail.com>
2014-01-12 18:22:25 -0500

    DMC - optimize for space and Pentium Pro

--- src/build-dmc.cmd
+++ src/build-dmc.cmd
@@ -14,6 +14,6 @@ flex.exe -i -Pparser3 -oparser3.c parser3.l
 bison.exe -d -v core.y -ocore.c
 flex.exe -i -olex.c lex.l
 
-dmc.exe -A -r -w- -gp -s asmsx.c wav.c core.c lex.c parser1.c parser2.c parser3.c > asmsx.dmc.err
+dmc.exe -A -r -w- -gp -s -o+space -6 asmsx.c wav.c core.c lex.c parser1.c parser2.c parser3.c > asmsx.dmc.err
 
 del core.c core.h lex.c parser?.c *.obj *.i *.output *.map
